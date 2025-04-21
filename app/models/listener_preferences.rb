2f75feb2d83cf1e4e886c537bf2d6a97b8ab8f77
jramdass <jramdass@torontomu.ca>
2025-04-21 15:27:57 -0400

    Add user search

--- app/models/listener_preferences.rb
+++ app/models/listener_preferences.rb
@@ -2,4 +2,8 @@ class ListenerPreferences < ApplicationRecord
   def self.filter_genre(filter)
     where("top_genre = ?", "#{filter}")
   end
+
+  def self.search(id)
+    where("user_id = ?", "#{id}")
+  end
 end
